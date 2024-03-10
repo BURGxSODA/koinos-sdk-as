8de0bee2b38656c3e39c25c455eafbc176f8814e
burger&fries <161644016+BURGxSODA@users.noreply.github.com>
2024-03-10 08:11:22 +0000

    Update configure_project.sh

--- docker/build_helpers/configure_project.sh
+++ docker/build_helpers/configure_project.sh
@@ -2,5 +2,5 @@
 
 cd /opt/koinos-sdk/as/project
 version=`cat /tmp/package.json | jq '.version'`
-yarn add --dev @koinos/sdk-as@${version:1:-1}
-yarn install
+yarn add --net @koinos/sdk-as@${version:1:-1}
+yarn install 
